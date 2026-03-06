077037ee648db73efdc23bc2cf0fdf5fe6cfb0bd
maxjohn81 <rivoandriharisoa@gmail.com>
2026-03-06 05:41:52 +0300

    Tache terminée

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -6,22 +6,31 @@ const SECRET_KEY = "monsecret123";
 
 const login = async (data) => {
   const { email, password } = data;
-  
+
   const admin = await prisma.administrateurs.findUnique({
     where: { email }
   });
-  
+
   if (!admin) {
     throw new Error("Email incorrect");
   }
-  
+
   const passwordIsValid = bcrypt.compareSync(password, admin.mot_de_passe);
   if (!passwordIsValid) {
     throw new Error("Mot de passe incorrect");
   }
-  
-  const token = jwt.sign({ email: admin.email }, SECRET_KEY, { expiresIn: "1h" });
-  return { token };
+
+  const token = jwt.sign({ email: admin.email, id: admin.id }, SECRET_KEY, { expiresIn: "7d" });
+
+
+  return {
+    token,
+    admin: {
+      id: admin.id,
+      email: admin.email,
+      nom: admin.nom
+    }
+  };
 };
 
 module.exports = {
